986025688ef472169efbe956810aaebdd7e203a3
marc durand <mjm.durand@hotmail.fr>
2023-03-18 00:42:58 +0100

    🐛 fix: missing encoding directives

--- copy-disk.ps1
+++ copy-disk.ps1
@@ -51,6 +51,15 @@ catch {
     # Update guestinfo.network
     Get-AdvancedSetting -Entity $vm -Name "guestinfo.metadata" | Set-AdvancedSetting -Value "$cloudInitNetworkFile" -Confirm:$false
 }
+fix:try {
+    New-AdvancedSetting -Entity $vm -Name "guestinfo.metadata.encoding" -Value "base64" -Confirm:$false -ErrorAction Stop
+}
+catch {
+    write-host "guestinfo.metadata.encoding already exists, updating it instead"
+    # Update guestinfo.network
+    Get-AdvancedSetting -Entity $vm -Name "guestinfo.metadata.encoding" | Set-AdvancedSetting -Value "base64" -Confirm:$false
+}
+
 
 # guestinfo.userdata
 try {
@@ -60,4 +69,12 @@ catch {
     write-host "guestinfo.userdata already exists, updating it instead"
     # Update guestinfo.network
     Get-AdvancedSetting -Entity $vm -Name "guestinfo.userdata" | Set-AdvancedSetting -Value "$cloudInitUserdataFile" -Confirm:$false
+}
+try {
+    New-AdvancedSetting -Entity $vm -Name "guestinfo.userdata.encoding" -Value "base64" -Confirm:$false -ErrorAction Stop
+}
+catch {
+    write-host "guestinfo.userdata.encoding already exists, updating it instead"
+    # Update guestinfo.network
+    Get-AdvancedSetting -Entity $vm -Name "guestinfo.userdata.encoding" | Set-AdvancedSetting -Value "base64" -Confirm:$false
 }
